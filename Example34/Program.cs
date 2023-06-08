7a2f7d743f44559c13645e35fde59a0d4d849d66
Olga <olechka.mir28@mail.ru>
2023-06-08 07:30:44 +0300

    Example34

---
+++ Example34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int EvenNumbersInArray(int[] array)
+{   
+    int sum = 0;
+    foreach (int el in array)
+    {
+      if (el % 2 == 0) sum +=1;
+    }
+    return sum;
+}
+
+Console.Clear();
+int[] array = GetArray(6, 100, 999);
+Console.WriteLine(String.Join(" ", array));
+int sum = EvenNumbersInArray(array);
+Console.WriteLine($"Количество четных чисел в массиве {sum}");
